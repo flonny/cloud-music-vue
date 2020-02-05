ffe7e0bf7f8a6c0acc0e4c39740a0975b892034e
范跃东 <632296950@qq.com>
2020-02-05 23:14:38 +0800

    slider components

--- src/application/Recommend/index.vue
+++ src/application/Recommend/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    recommend
+    <Slider :banner-list="bannerList" />
   </div>
 </template>
+<script>
+import Slider from '../../components/slider/index.vue';
+
+export default {
+  components: {
+    Slider,
+  },
+  data() {
+    return {
+      bannerList: [1, 2, 3, 4].map((item) => ({
+        imageUrl: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
+        index: item,
+      })),
+    };
+  },
+};
+</script>
---
+++ src/components/slider/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="SliderContainer">
+    <div class="before" />
+    <div
+      ref="slider-container"
+      class="slider-container"
+    >
+      <div
+        class="swiper-wrapper"
+      >
+        <div
+          v-for="slider in bannerList"
+          :key="slider.index"
+          class="swiper-slide"
+        >
+          <div class="slider-nav">
+            <img
+              :src="slider.imageUrl"
+              width="100%"
+              height="100%"
+              alt="推荐"
+            >
+          </div>
+        </div>
+      </div>
+      <div
+        ref="swiper-pagination"
+        class="swiper-pagination"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import 'swiper/css/swiper.css';
+import Swiper from 'swiper';
+
+export default {
+  props: {
+    bannerList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      sliderSwiper: null,
+    };
+  },
+  mounted() {
+    if (this.bannerList.length && !this.sliderSwiper) {
+      this.sliderSwiper = new Swiper('.slider-container', {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: { el: '.swiper-pagination' },
+      });
+    }
+  },
+};
+</script>
+<style lang="stylus" >
+@import './style'
+</style>
